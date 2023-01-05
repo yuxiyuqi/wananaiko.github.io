3a29a36fddb6e0ca974e2dfc13f8052cc66d57c9
wananaiko <zouyunpeng5152@gmail.com>
2023-01-06 01:37:52 +0800

    test

--- content/memos/assets/js/bibi.js
+++ content/memos/assets/js/bibi.js
@@ -249,11 +249,3 @@ bbDom && (
 );
 // Images lightbox
 window.ViewImage && ViewImage.init('.container img');
-
-// 设置全部链接在新标签页打开
-window.onload = function() {
-    var links = document.getElementsByTagName("a");
-    for (var i = 0; i < links.length; i++) {
-      links[i].target = "_blank";
-    }
-};
