eefc296ac00a87583ac17f299b2b399e0da584ea
wananaiko <zouyunpeng5152@gmail.com>
2023-06-04 01:34:21 +0800

    update

--- content/memos/assets/js/bibi.js
+++ content/memos/assets/js/bibi.js
@@ -342,11 +342,3 @@ window.addEventListener("scroll", function () {
     btn && btn.click();
   }
 });
-
-// 将lately.js替换为使用moment.min.js来显示时间.
-var Lately = {
-  format: function (e) {
-    var t = moment(e);
-    return t.fromNow();
-  },
-};
